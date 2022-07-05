cc20b3e446e06c89945ad1c275f2e3ba5a940a47
omidrezasalari <omidsalari1995@gmail.com>
2022-07-05 14:57:46 +0430

    [CONFIG]- Add rabbitMq config

--- Modules/DelayReport/src/config/delivery.php
+++ Modules/DelayReport/src/config/delivery.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
-    "refresh-time-url"=>"https://run.mocky.io/v3/122c2796-5df4-461c-ab75-87c1192b17f7"
+    /*
+    |--------------------------------------------------------------------------
+    | RabbitMq config for connect to server
+    |--------------------------------------------------------------------------
+    */
+    'rabbitmq_host' => "localhost",
+    "rabbitmq_port" => 5672,
+    "rabbitmq_username" => "guest",
+    "rabbitmq_password" => "guest",
+    "rabbitmq_queue_name" => "delay_queue",
+    "rabbitmq_vhost" => "/",
+    "exchange_name" => "snapp-food",
+    "exchange_type" => "direct",
+    "exchange_serverity" => "DELAY",
+    "delivery_mode" => PhpAmqpLib\Message\AMQPMessage::DELIVERY_MODE_PERSISTENT,
+
+
+    "refresh-time-url" => "https://run.mocky.io/v3/122c2796-5df4-461c-ab75-87c1192b17f7"
+
+
+
 ];
