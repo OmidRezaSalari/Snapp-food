0bfe11db4f8d567e4280052264f09a8747e6c61a
omidrezasalari <omidsalari1995@gmail.com>
2022-07-05 23:32:35 +0430

    [TEST]- Add tests for assign delay report to agent

--- Modules/DelayReport/src/Database/Factories/VendorFactory.php
+++ Modules/DelayReport/src/Database/Factories/VendorFactory.php
@@ -2,14 +2,14 @@
 
 namespace DelayReport\Database\Factories;
 
-use DelayReport\Models\Order;
+use DelayReport\Models\Vendor;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 
-class OrderFactory extends Factory
+class VendorFactory extends Factory
 {
 
-    protected $model = Order::class;
+    protected $model = Vendor::class;
     /**
      * Define the model's default state.
      *
@@ -18,8 +18,7 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
-            'owner_id' => random_int(1, 10),
-            'time_delivery' => random_int(1, 50)
+            'name' => $this->faker->name(),
         ];
     }
 }
--- Modules/DelayReport/tests/DelayReportTest.php
+++ Modules/DelayReport/tests/DelayReportTest.php
@@ -2,11 +2,15 @@
 
 namespace DelayReportTest;
 
+use DelayReport\Facades\DelayReport\DelayReportProviderFacade;
+use DelayReport\Facades\Message\MessageSenderFacade;
 use DelayReport\Facades\Response\ResponderFacade;
 use DelayReport\Facades\Trip\TripProviderFacade;
 use DelayReport\Facades\TripHandler\DeliverdTripHandler;
 use DelayReport\Facades\TripHandler\InProcessTripHandler;
 use DelayReport\Facades\TripHandler\TripHandlerFacade;
+use DelayReport\Models\Order;
+use DelayReport\Models\Vendor;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -25,12 +29,17 @@ class AuthenticateTest extends TestCase
     public function user_can_report_order_delay_when_order_has_not_trip()
     {
 
+        $orderId = Order::factory()
+            ->count(1)
+            ->for(Vendor::factory())->create()->first()->id;
+
+
         TripProviderFacade::shouldReceive("isValidTrip")
-            ->with(1)->once()->andReturnNull();
+            ->with($orderId)->once()->andReturnNull();
 
         $message = __("DelayReportService::message.send-to-delay-queue");
         TripHandlerFacade::shouldProxyTo(DeliverdTripHandler::class);
-        TripHandlerFacade::shouldReceive("handle")->with(1)->once()
+        TripHandlerFacade::shouldReceive("handle")->with($orderId)->once()
             ->andReturn($message);
 
 
@@ -38,7 +47,7 @@ class AuthenticateTest extends TestCase
             ->with($message)->once()
             ->andReturn(response()->json(["message" => $message]));
 
-        $res = $this->postJson(route('v1.orders.delays.add-new-report'), ['orderId' => 1]);
+        $res = $this->postJson(route('v1.orders.delays.add-new-report'), ['orderId' => $orderId]);
 
         $res->assertSuccessful()->assertJsonFragment(["message" => $message]);
     }
@@ -47,8 +56,12 @@ class AuthenticateTest extends TestCase
     public function user_can_report_order_delay_when_order_has_a_trip()
     {
 
+        $orderId = Order::factory()
+            ->count(1)
+            ->for(Vendor::factory())->create()->first()->id;
+
         TripProviderFacade::shouldReceive("isValidTrip")
-            ->with(1)->once()->andReturnNull();
+            ->with($orderId)->once()->andReturnNull();
 
         $message = __(
             "DelayReportService::message.predict-time-to-delivery",
@@ -56,7 +69,7 @@ class AuthenticateTest extends TestCase
         );
 
         TripHandlerFacade::shouldProxyTo(InProcessTripHandler::class);
-        TripHandlerFacade::shouldReceive("handle")->with(1)->once()
+        TripHandlerFacade::shouldReceive("handle")->with($orderId)->once()
             ->andReturn($message);
 
 
@@ -64,8 +77,44 @@ class AuthenticateTest extends TestCase
             ->with($message)->once()
             ->andReturn(response()->json(["message" => $message]));
 
-        $res = $this->postJson(route('v1.orders.delays.add-new-report'), ['orderId' => 1]);
+        $res = $this->postJson(route('v1.orders.delays.add-new-report'), ['orderId' => $orderId]);
 
         $res->assertSuccessful()->assertJsonFragment(["message" => $message]);
+    }
+
+    /** @test */
+    public function when_assign_delay_report_to_agent_successfully()
+    {
+
+        MessageSenderFacade::shouldReceive("received")->withNoArgs()->once()->andReturn(1);
+
+        DelayReportProviderFacade::shouldReceive("accessToAgent")->with(1)->once();
+
+        $message = __("DelayReportService::message.access-to-agent");
+
+        ResponderFacade::shouldReceive("accessToAgentSuccessfully")->withNoArgs()
+            ->once()->andReturn(response()->json(['message' => $message]));
+
+        ResponderFacade::shouldReceive("queueIsEmpty")->never();
+
+        $res = $this->postJson(route('v1.orders.forReviews'));
+    }
+
+    /** @test */
+    public function when_queue_is_empty_when_assign_delay_report_to_agent()
+    {
+
+        MessageSenderFacade::shouldReceive("received")->withNoArgs()->once()->andReturnNull();
+
+        DelayReportProviderFacade::shouldReceive("accessToAgent")->never();
+
+        $message = __("DelayReportService::client-error.delay-queue-is-empty");
+
+        ResponderFacade::shouldReceive("accessToAgentSuccessfully")->never();
+
+        ResponderFacade::shouldReceive("queueIsEmpty")->withNoArgs()
+            ->once()->andReturn(response()->json(['message' => $message]));
+
+        $res = $this->postJson(route('v1.orders.forReviews'));
     }
 }
